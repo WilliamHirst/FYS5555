c1f8234fd3ba2a6a7cd9edbf1cef0f32bc6ef872
William Hirst <bod_holthe@outlook.com>
2022-05-03 14:15:54 +0200

    Recontructed train, val and test data sets.

--- Proj3/code/Utilities.py
+++ Proj3/code/Utilities.py
@@ -12,6 +12,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import roc_curve,auc
 from scipy.stats import randint as sp_randint
 from scipy.stats import uniform as sp_uniform
+from sklearn.utils import shuffle
 from sklearn.model_selection import RandomizedSearchCV 
 
 # Keras tools
@@ -78,19 +79,82 @@ def ask(q):
             state = False
     return isYes
 
+
+
 def splitData(X, Y, split):
-    X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=split, random_state=2)
-    s = X_train[Y_train==1]
-    b = X_train[Y_train==0]
+    X,Y = shuffle(X,Y, random_state=2)
 
-    indx = random.sample(range(len(b)), len(s))
-    re_indx = [i for i in range(len(b)) if i not in indx]
+    x_s = X[Y == 1]
+    x_b = X[Y == 0]
+    y_s = Y[Y == 1] 
+    y_b = Y[Y == 0]
 
-    W_train = X[indx,-1]
-    W_val = X[re_indx,-1]
-    X_val = np.concatenate(X_val[:,-1], X_train[re_indx,-1], axis=0)
-    Y_val = np.concatenate(Y_val, Y_train[re_indx], axis=0)
-    X_train = X_train[indx,:-1]
-    Y_train = Y_train[indx]
-    
-    return X_train, X_val, Y_train, Y_val, W_train, W_val
+
+    rng = np.random.default_rng(seed=2)
+    indx = rng.choice(len(x_b), len(x_s), replace=False)
+    re_indx = [i for i in range(len(x_b)) if i not in indx]
+
+    split_indx  = int(len(x_s)*split)
+   
+    X_train = np.concatenate((x_s[:,:-1], x_b[indx,:-1]), axis=0)[split_indx:]
+    Y_train = np.concatenate((y_s, y_b[indx]))[split_indx:]
+    W_train = np.concatenate((x_s[:,-1], x_b[indx,-1]))[split_indx:]
+    W_train *= len(X_train)/len(Y)
+
+    X_val = shuffle(X_train, random_state=2)[0:split_indx]
+    Y_val = shuffle(Y_train, random_state=2)[0:split_indx]
+    W_val = shuffle(W_train, random_state=2)[0:split_indx]
+    W_val *= len(X_val)/len(Y)
+
+    W_test = x_b[re_indx,-1] 
+    X_test = x_b[re_indx,:-1]
+    Y_test = y_b[re_indx]
+    W_test *= len(X_test)/len(Y)
+
+
+    return X_train, X_val, X_test, Y_train, Y_val, Y_test, W_train, W_val, W_test
+
+def plotHistoBS(y_b, y_s, w_b, w_s, name, title,  nrBins = 15):
+    plt.figure(num=0, dpi=80, facecolor='w', edgecolor='k')
+    n, bins, patches = plt.hist(y_b, bins = np.linspace(0,1.,nrBins), facecolor='blue', alpha=0.2,label="Background", weights = w_b)#,density=True)#, density=True)
+    n, bins, patches = plt.hist(y_s, bins = np.linspace(0,1.,nrBins), facecolor='red', alpha=0.2, label="Signal", weights = w_s)#,density=True)#, density=True)
+    plt.xlabel('XGBoost output',fontsize=14)
+    plt.ylabel('Events',fontsize=14)
+    plt.title(title,fontsize=14)
+    plt.grid(True)
+    plt.legend(fontsize=12)
+    plt.yscale('log')
+    if ask("Save Image? [y/n]"):
+        plt.savefig(f"{name}", bbox_inches="tight")
+    plt.show()
+
+def plotHistoB(y_b, w_b, name, title,  nrBins = 15):
+    plt.figure(num=0, dpi=80, facecolor='w', edgecolor='k')
+    n, bins, patches = plt.hist(y_b, bins = np.linspace(0,1.,nrBins), facecolor='blue', alpha=0.6,label="Background", weights = w_b)#,density=True)#, density=True)
+    plt.xlabel('XGBoost output',fontsize=14)
+    plt.ylabel('Events',fontsize=14)
+    plt.title(title,fontsize=14)
+    plt.grid(True)
+    plt.legend(fontsize=12)
+    plt.yscale('log')
+    if ask("Save Image? [y/n]"):
+        plt.savefig(f"{name}", bbox_inches="tight")
+    plt.show()
+
+
+def plotRoc(Y, Y_pred, title):
+    fpr, tpr, thresholds = roc_curve(Y,Y_pred[:,1], pos_label=1)
+    roc_auc = auc(fpr,tpr)
+    lw = 2
+
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange',
+            lw=lw, label='ROC curve (1-area = %0.2e)' % (1.-roc_auc))
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(title)
+    plt.legend(loc="lower right")
+    plt.show()
--- Proj3/code/XGB.py
+++ Proj3/code/XGB.py
@@ -12,12 +12,13 @@ def gridSearch(X,Y):
                                 objective='binary:logistic',
                                 missing=-999.0,
                                 use_label_encoder=False) 
-    param_dist = {"n_estimators": sp_randint(1, 150),
-              "max_depth": sp_randint(1,6),
+    param_dist = {"n_estimators": sp_randint(1, 200),
+              "max_depth": sp_randint(1,20),
               "learning_rate": sp_uniform(0.1,1.0),
               "gamma": sp_uniform(0,10),
               "min_child_weight": sp_uniform(0,10),
              }
+
 
     # run random search
     n_iter_search = 50
@@ -51,21 +52,21 @@ nrS = np.sum(Y == 1)
 nrB = sum(Y == 0)
 print(f"|   B: {nrB:.2e}   S: {nrS:.2e}   |")
 
-X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2, random_state=2)
-
-w_train = X_train[:,-1]
-w_val = X_val[:,-1]
-
-exit()
-X_train, X_val = X_train[:,:-1], X_val[:,:-1]
+X_train, X_val, X_test, Y_train, Y_val, Y_test, W_train, W_val, W_test= splitData(X,Y,0.2)
 
 # Weights
-wsig_v = [w_val[i] for i in range(len(Y_val)) if Y_val[i] == 1.0 ]
-wbkg_v = [w_val[i] for i in range(len(Y_val)) if Y_val[i] == 0.0 ]
-wsig = [w_train[i] for i in range(len(Y_train)) if Y_train[i] == 1.0 ]
-wbkg = [w_train[i] for i in range(len(Y_train)) if Y_train[i] == 0.0 ]
+wbkg_t = [W_test[i] for i in range(len(Y_test)) if Y_test[i] == 0.0 ]
+
+wsig = [W_train[i] for i in range(len(Y_train)) if Y_train[i] == 1.0 ]
+wbkg = [W_train[i] for i in range(len(Y_train)) if Y_train[i] == 0.0 ]
+
+wsig_v = [W_val[i] for i in range(len(Y_val)) if Y_val[i] == 1.0 ]
+wbkg_v = [W_val[i] for i in range(len(Y_val)) if Y_val[i] == 0.0 ]
+
+
 sum_wsig = sum( wsig )
 sum_wbkg = sum( wbkg )
+
 
 if preformGS:
     global XSize 
@@ -88,42 +89,49 @@ else:
                         eval_metric="error") 
 
     print("|          Training...          |")
-
-    xgb.fit(X_train,Y_train) 
+    xgb.fit(X_train,Y_train)
 
     print("|        Finished traing        |")
     print(" ------------------------------- ")
 timer(start_time)
 
-y_pred_prob = xgb.predict_proba( X_val ) 
+"""
+TRAINING DATA
+"""
+y_pred = xgb.predict_proba( X_train ) 
+y_b = y_pred[:,1][Y_train==0]
+y_s = y_pred[:,1][Y_train==1]
+name = "../figures/XGB/train.pdf"
+title =  "XGB output, MC-data, training data"
+plotHistoBS(y_b, y_s, wbkg, wsig, name, title,  nrBins = 15)
 
-plt.figure(num=0, dpi=80, facecolor='w', edgecolor='k')
-n, bins, patches = plt.hist(y_pred_prob[:,1][Y_val==0], bins = np.linspace(0,1.,15), facecolor='blue', alpha=0.2,label="Background", weights = wbkg_v)#,density=True)#, density=True)
-n, bins, patches = plt.hist(y_pred_prob[:,1][Y_val==1], bins = np.linspace(0,1.,15), facecolor='red', alpha=0.2, label="Signal", weights = wsig_v)#,density=True)#, density=True)
-plt.xlabel('XGBoost output',fontsize=14)
-plt.ylabel('Events',fontsize=14)
-plt.title('XGBoost output, MC-data, validation data',fontsize=14)
-plt.grid(True)
-plt.legend(fontsize=12)
-plt.yscale('log')
-if ask("Save Image? [y/n]"):
-    plt.savefig("../figures/XGB/OutDistExtraCuts.pdf", bbox_inches="tight")
-plt.show()
+title = "ROC for XGB on MC-dataset (training)"
 
-fpr, tpr, thresholds = roc_curve(Y_val,y_pred_prob[:,1], pos_label=1)
-roc_auc = auc(fpr,tpr)
-plt.figure()
-lw = 2
-plt.plot(fpr, tpr, color='darkorange',
-        lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
-plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('ROC for xgBoost on MC-dataset')
-plt.legend(loc="lower right")
-plt.show()
+plotRoc(Y_train, y_pred, title)
+
+"""
+VALIDATION DATA
+"""
+y_pred = xgb.predict_proba( X_val ) 
+y_b = y_pred[:,1][Y_val==0]
+y_s = y_pred[:,1][Y_val==1]
+name = "../figures/XGB/val.pdf"
+title =  "XGB output, MC-data, validation data"
+plotHistoBS(y_b, y_s, wbkg_v, wsig_v, name, title,  nrBins = 15)
+
+title = "ROC for XGB on MC-dataset (valdiation)"
+plotRoc(Y_val, y_pred, title)
+
+
+"""
+TEST DATA
+"""
+y_b = xgb.predict_proba( X_test )[:,1]
+
+name = "../figures/XGB/test.pdf"
+title =  "XGB output, MC-data, test data"
+plotHistoB(y_b, wbkg_t, name, title,  nrBins = 15)
+
 
 
 if  ask("Do you want to save model? (y/n) "):
